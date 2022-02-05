edfb90a2a75b76af4b83a0fbac78ef4b9d609d40
ryan-lam <ryan-lam@users.noreply.github.com>
2022-02-05 17:28:30 -0500

    fixed bugs with backend and routing

--- backend/routes/scheduler.js
+++ backend/routes/scheduler.js
@@ -1,15 +1,32 @@
 // Modules
 const express = require("express")
-const router = require("express").Router
+const router = express.Router()
 const {v4} = require("uuid")
-// Middleware + parser
-router.use(cors())
-router.use(bodyParser.json())
+
 // Database
 var admin = require("firebase-admin");
+const { NetworkContext } = require("twilio/lib/rest/supersim/v1/network")
 const db = admin.firestore()
 const testDB = db.collection("test")
 const employeeDB = db.collection("Employees")
 
 
-module.export = router
+
+
+router.get("/:date", (req, res) => {
+    console.log("hit")
+    const date = req.params.date
+    console.log(date)
+    return res.send(date)
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = router
