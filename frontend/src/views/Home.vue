7ae41bf392a6d40b325e682752aa258cdbd435f0
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-02-05 23:53:48 -0500

    Added profiles

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,19 @@
   
   <main class="board">
     <h1>Schedule</h1>
-	<section class=""></section>
+	<section class="list">
+		<div style="height: 40px"></div>
+		<div class="list--item">
+			<div class="list--item__profile">
+				<img src="./../assets/portrait.png" alt="">
+				<h2>Ryan Lam</h2>
+			</div>
+			<p>Start Time: 8:00</p>
+			<p>End Time: 17:00</p>
+		</div>
+		<div class="item"></div>
+		<div class="item"></div>
+	</section>
   </main>
 </template>
 
@@ -16,11 +28,19 @@ import Sidebar from '@/components/Sidebar.vue'
 
 
 export default {
-  name: 'Home',
-  components: {
-    Sidebar
+	name: 'Home',
+	components: {
+		Sidebar
 
-  }
+	},
+	data(){
+		return{
+			working: [
+
+			]
+		}
+	},
+
 }
 </script>
 
@@ -43,15 +63,48 @@ main{
 		font-size: 50px;
 	}
 
-	& > section{
-		width: 85%;
-		margin: auto;
-		height: 500px;
-		border-radius: 30px;
-		background-color: white;
-		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.301);
-		margin-top: 40px;
+}
 
+.list{
+	width: 85%;
+	margin: auto;
+	height: 500px;
+	border-radius: 30px;
+	background-color: white;
+	box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.301);
+	margin-top: 40px;
+
+	&--item{
+		width: 90%;
+		margin: auto;
+		margin-top: 20px;
+		height: 80px;
+		border-radius: 15px;
+		background-color: rgb(219, 242, 255);
+		border: 3px solid $green;
+		// box-shadow: 0px 0px 3px 0px rgba(211, 211, 211, 0.637);
+		// box-shadow: 0px 0px 3px 1px $green;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: center;
+		
+		&__profile{
+			display: flex;
+			flex-direction: row;
+			transform: translateX(-30px);
+			align-items: center;
+			& > img{
+				display: block;
+				height: 50px;
+			}
+			& > h2{
+				margin-left: 20px;
+			}
+
+		}
+		
 	}
+
 }
 </style>
