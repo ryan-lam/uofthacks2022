4f98ff460a8102a97b83515f696293e444b57d0a
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-02-05 19:15:39 -0500

    Started sidebar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+  <Sidebar/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Sidebar from '@/components/Sidebar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+@import "./../assets/styles.scss";
+
+</style>
