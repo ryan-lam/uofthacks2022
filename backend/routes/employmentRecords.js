a8f65b93056e29065cb27a205f33bdeed3b70530
TK081 <tiseagan2001@gmail.com>
2022-02-05 23:14:42 -0500

    Update employmentRecords.js

--- backend/routes/employmentRecords.js
+++ backend/routes/employmentRecords.js
@@ -5,7 +5,8 @@ const {v4} = require("uuid")
 
 // Database
 var admin = require("firebase-admin");
-const { NetworkContext } = require("twilio/lib/rest/supersim/v1/network")
+const { NetworkContext } = require("twilio/lib/rest/supersim/v1/network");
+const { route } = require("./scheduler");
 const db = admin.firestore()
 const testDB = db.collection("test")
 const employeeDB = db.collection("employee")
@@ -15,7 +16,22 @@ const recruitingeDB = db.collection("recruiting")
 const employmentRecordsDB = db.collection("employment-records")
 
 
-
+//
+router.post("/create-employee", async (req, res) => {
+    const {id, first, last, address, DOB, age, hours, earnings} = req.body; 
+    const employee = employmentRecordsDB.doc(id);
+    await employee.set({
+        id: id,
+        first: first,
+        last: last,
+        address: address,
+        DOB: DOB,
+        age: age, 
+        hours: hours,
+        earnings: earnings
+    },{merge:true})
+    return res.json({success:true})
+})
 
 
 
